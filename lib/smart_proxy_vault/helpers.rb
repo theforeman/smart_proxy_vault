6f1ce9ca6492e5121358860ada53f2ccfc4270d9
Riley Shott <rileyshott+git@gmail.com>
2016-04-05 08:21:02 -0700

    Added helper module for validating TTL parameter

---
+++ lib/smart_proxy_vault/helpers.rb
@@ -0,0 +1,31 @@
+module VaultPlugin
+  module Helpers
+    def settings_ttl
+      ::VaultPlugin::Plugin.settings[:token_options][:ttl]
+    end
+
+    def to_seconds(string)
+      case string.slice(-1)
+      when 'd'
+        string.tr('d', '').to_i * 30 * 24
+      when 'h'
+        string.tr('h', '').to_i * 3600
+      when 'm'
+        string.tr('m', '').to_i * 60
+      when 's'
+        string.tr('s', '').to_i
+      else
+        log_halt 400, "Invalid TTL - #{string}. Must end with 'd', 'h', 'm' or 's'."
+      end
+    end
+
+    # Only allow clients to specify a TTL that is shorter than the default
+    def valid_ttl?(ttl)
+      return true if ttl.nil? || settings_ttl.nil?
+      unless (to_seconds(settings_ttl) >= to_seconds(ttl))
+        log_halt 400, "Invalid TTL - #{ttl}. Must be shorter or equal to #{settings_ttl}."
+      end
+      true
+    end
+  end
+end
